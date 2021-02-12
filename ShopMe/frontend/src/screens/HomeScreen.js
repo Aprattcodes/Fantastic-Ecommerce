374a1390c6ae04eb0d381b5f874c876d3d3371d8
Alyssa Pratt <alyssakpratt@gmail.com>
2021-02-12 10:46:19 -0600

    adds loading screen icon

--- ShopMe/frontend/src/screens/HomeScreen.js
+++ ShopMe/frontend/src/screens/HomeScreen.js
@@ -1,29 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import Product from '../components/Product'; //two dots back two folders
+import LoadingBox from '../components/LoadingBox';
+import MessageBox from '../components/MessageBox';
 
 
 export default function HomeScreen() {
   const [products, setProducts] = useState([]); //uses react hook to manage react component (below)
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+
   useEffect(() =>{
     const fetchData = async () => {
-      const {data} = await axios.get('/api/products');
-      setProducts(data);
+      
+      try{
+        setLoading(true);
+        const {data} = await axios.get('/api/products');
+        setLoading(false);
+        setProducts(data);
+      } catch(err){
+        setError(err.message);
+        setLoading(false); //if there is an error in loading api/product set error to error hook
+      }
+      
     };
     fetchData();
   }, [])
+
     return (
         <div>
-        <div className="row center">
-
-          {
-            products.map((products) => (
-            <Product key={products._id} products={products}></Product> //extracts a component
-              )
-            )
-          }
-
-        </div> 
-      </div>
+          {loading? (
+          <LoadingBox></LoadingBox>
+          ) : error? (
+            <MessageBox variant="danger">{error}</MessageBox>
+          ):(
+            <div className="row center">
+              {products.map((products) => (
+                <Product key={products._id} products={products}></Product> //extracts a component
+              ))}
+            </div> 
+            )}        
+        </div>
         );
 }
